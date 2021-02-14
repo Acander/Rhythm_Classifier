4960dcdff939a96b92987f075d06ae04b0a68a7b
Acander <adrianz@kth.se>
2021-02-14 12:45:52 +0100

    Extract tempogram

---
+++ Pre-processing.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import madmom as mm
+import librosa as lib
+import dataHandler as dh
+import scipy.signal as sps
+
+
+def plot_waveform(samples, title, sample_rate=0.0):
+    # The following makes the plot look nice
+    params = {'legend.fontsize': 'x-large',
+              'figure.figsize': (15, 5),
+              'axes.labelsize': 'x-large',
+              'axes.titlesize': 'x-large',
+              'xtick.labelsize': 'x-large',
+              'ytick.labelsize': 'x-large'}
+    plt.rcParams.update(params)
+
+    x = np.arange(len(samples))
+    if sample_rate:
+        x = x / sample_rate
+    plt.plot(x, samples)
+    plt.ylabel('Amplitude')
+    plt.xlabel('Time(s)')
+    plt.title(title)
+    plt.show()
+
+
+def load_samples_from_text(class_name):
+    dataset = np.load(class_name + '.npy')
+    return dataset
+
+
+def gen_tempogram(sample):
+    sample_len = len(sample) / dh.SAMPLE_RATE
+    spec = mm.audio.spectrogram.Spectrogram(sample)
+    sf = mm.features.onsets.spectral_diff(spec)
+    sf_sample_rate = len(
+        sf) / sample_len  # Divde the sample length (in seconds) by n points in the spectral flux to get spectral flux sample_rate. (Inverse)
+    print(sf_sample_rate * sample_len)
+    plot_waveform(sf, sample_rate=sf_sample_rate, title="Spectral Flux")
+    tempogram = fourier_transform(sf, sf_sample_rate)
+    return tempogram
+
+
+def fourier_transform(onsets, sample_rate):
+    win_size = sample_rate * 8
+    overlap = int(win_size - sample_rate * 0.5)
+    freq, time, stft = sps.stft(onsets, fs=sample_rate, nperseg=win_size, noverlap=overlap,
+                                nfft=8192, window='hann')
+    tempo = hertz_to_tempo(freq)
+    magnitudes = np.abs(stft)
+    plot_tempogram(magnitudes, time, tempo)
+
+    return magnitudes
+
+
+def plot_tempogram(magnitudes, time, tempo):
+    cmap = plt.get_cmap('copper')
+    plt.ylim([25, 200])
+    plt.pcolormesh(time, tempo, magnitudes, cmap=cmap)
+    plt.set_cmap(cmap)
+    plt.title('Tempogram')
+    plt.ylabel('BPM')
+    plt.xlabel('Time (s)')
+    plt.show()
+
+
+def hertz_to_tempo(frequencies):
+    return 60 * frequencies  # Formula 6.24
+
+
+if __name__ == '__main__':
+    dataset = load_samples_from_text(dh.FOLDER_NAMES[0])
+    plot_waveform(dataset[0], sample_rate=dh.SAMPLE_RATE, title=dh.FOLDER_NAMES[0])
+    gen_tempogram(sample=dataset[0])
--- dataHandler.py
+++ dataHandler.py
@@ -1,4 +1,4 @@
-# This class file contains functions for retrieving the waveforms from the mp3 files and generates a dataset.
+# This class file contains functions for retrieving the waveforms from the mp3 files.
 # The dataset used is the extended ballroom dataset
 
 import matplotlib.pyplot as plt
@@ -6,9 +6,9 @@ import numpy as np
 import librosa as lib
 import os
 
-data_set_folder = "C:/Users/adria/PycharmProjects/Rhythm_Classifier/extendedballroom_v1.1/"
+DATA_SET_HANDLER = "C:/Users/adria/PycharmProjects/Rhythm_Classifier/extendedballroom_v1.1/"
 
-folder_names = [
+FOLDER_NAMES = [
     "Chacha",
     "Foxtrot",
     "Jive",
@@ -24,23 +24,7 @@ folder_names = [
     "Wcswing"
 ]
 
-
-def plot_waveform(samples, sample_rate, title):
-    # The following makes the plot look nice
-    params = {'legend.fontsize': 'x-large',
-              'figure.figsize': (15, 5),
-              'axes.labelsize': 'x-large',
-              'axes.titlesize': 'x-large',
-              'xtick.labelsize': 'x-large',
-              'ytick.labelsize': 'x-large'}
-    plt.rcParams.update(params)
-
-    x = np.arange(len(samples)) / sample_rate
-    plt.plot(x, samples)
-    plt.ylabel('Amplitude')
-    plt.xlabel('Time(s)')
-    plt.title(title)
-    plt.show()
+SAMPLE_RATE = 22050
 
 
 def load_sample(filename, time_s=None):
@@ -50,22 +34,22 @@ def load_sample(filename, time_s=None):
         samples = y.data[0:n_samples]
     else:
         samples = y.data
-    plot_waveform(samples, sample_rate, "Waveform")
+    print(sample_rate)
     return samples, sample_rate
 
 
-def load_samples(class_size=None, n_classes=len(folder_names)):
+def load_samples(class_size=None, n_classes=len(FOLDER_NAMES)):
     dataset = []
     for i in range(n_classes):
         print("Loading class: " + str(i))
         dance_class = []
-        for j, filename in enumerate(os.listdir(data_set_folder + folder_names[i])):
+        for j, filename in enumerate(os.listdir(DATA_SET_HANDLER + FOLDER_NAMES[i])):
             if class_size is not None:
                 if j > class_size:
                     break
-            music_sample, _ = load_sample(data_set_folder + folder_names[i] + "/" + filename)
+            music_sample, _ = load_sample(DATA_SET_HANDLER + FOLDER_NAMES[i] + "/" + filename)
             dance_class.append(np.array(music_sample))
-        save_samples_to_text(np.array(dance_class), folder_names[i])
+        save_samples_to_text(np.array(dance_class), FOLDER_NAMES[i])
         dataset.append(np.array(dance_class))
 
 
@@ -74,18 +58,11 @@ def save_samples_to_text(dataset, class_name):
     np.save(class_name, dataset)
 
 
-def load_samples_from_text(class_name):
-    dataset = np.load(class_name + '.npy')
-    print(dataset)
-    return dataset
-
-
 def test_load():
     test_file = "C:/Users/adria/PycharmProjects/Rhythm_Classifier/extendedballroom_v1.1/Chacha/100701.mp3"
     samples, sample_rate = load_sample(test_file)
-    plot_waveform(samples, sample_rate, "Chacha")
+    print(sample_rate)
 
 
 if __name__ == '__main__':
     dataset = load_samples(class_size=4, n_classes=2)
-    load_samples_from_text(folder_names[1])
